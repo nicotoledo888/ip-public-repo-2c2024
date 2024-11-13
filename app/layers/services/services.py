70005970919198c767520c81706628e193929e28
nicotoledo888 <nicotoledo888@gmail.com>
2024-11-13 09:10:40 -0300

    Actualizar services.py

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -3,10 +3,11 @@
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
-
+import transport
+from translate import 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
+    json_collection = transport.getAllImages(input)
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
